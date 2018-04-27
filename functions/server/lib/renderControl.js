d31683bc0ebbc547086e5fc976c591d10193f901
haryoenjang <markone@wemakeprice.com>
2018-04-27 16:14:22 +0900

    feat : extra page 정보 originalUrl > url pathname 변경

--- functions/server/lib/renderControl.js
+++ functions/server/lib/renderControl.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const url = require('url');
+
 module.exports = (req, res, next) => {
+  const parseUrl = url.parse(req.url);
+
   req.rc = {
     extra: {
-      page: req.originalUrl,
+      page: parseUrl.pathname,
+      params: req.query,
     },
   };
   next();
